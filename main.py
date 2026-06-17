5df932f42ba7b38f11a03906b5d00cad69db51d9
magarat <liam.neidig@gmail.com>
2026-06-17 08:56:26 -0500

    Added a player that can turn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
@@ -7,6 +8,9 @@ def main():
 	clock = pygame.time.Clock()
 	dt = 0.0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2
+	player1 = Player(x,y)
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,9 +20,11 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
+		player1.draw(screen)
+		player1.update(dt)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
-		print(dt)
+		# print(dt)
 
 if __name__ == "__main__":
     main()
